15ca220961d93cde445a17723bd7d3e4ec20a6f3
Dennis Jauernig <dennis@factorial.io>
2022-11-04 16:31:05 +0100

    fix: wrong enum, mark mediaFeature as optional

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -64,7 +64,7 @@ export interface Breakpoint {
 }
 
 export interface customProperty {
-  mediaFeature: "minWidth" | "maxWidth" | "resolution" | "mediaQuery";
+  mediaFeature?: "minWidth" | "maxWidth" | "resolution";
   name: string;
   value: string;
 }
